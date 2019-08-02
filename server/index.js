cdbee1e47dd26efca0d2cdae247e525de088b62c
tgnc <tgnc@yandex-team.ru>
2019-08-02 12:07:24 +0300

    добавила prettify

--- server/index.js
+++ server/index.js
@@ -5,6 +5,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/express_backend', (req, res) => {
-  const rr;
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
