4a9ea42d3fa08572a7b584c27ef5ac765ebe5e83
tgnc <tgnc@yandex-team.ru>
2019-08-02 21:09:37 +0300

    обработка ошибки подключения

--- server/database/mongoose.js
+++ server/database/mongoose.js
@@ -1,8 +1,11 @@
-
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1:27018/read-it-later', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
-})
+}).then(() => {
+    console.info('Mongo connected - ok');
+}).catch((e) => {
+    console.error('Mongo connection error: ', e);
+});
