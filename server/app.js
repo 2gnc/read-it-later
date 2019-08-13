0636957e2bd3715691119a059a5ceb14b5137c64
tgnc <tgnc@yandex-team.ru>
2019-08-13 18:06:03 +0300

    поменяла пути

--- server/app.js
+++ server/app.js
@@ -1,20 +1,18 @@
-const path = require('path')
+// const path = require('path')
 const express = require('express');
 // const config = require('./utils/config');
 const userData = require('./api/v1/user-data');
 
 require('./database/index');
 
-
-const publicPath = path.join(__dirname, './client/build');
-
 const app = express()
+    .use(express.static('../client/build'))
     .use(express.json())
     .use(userData)
     .get('*', (req, res) => {
-        return res.sendFile('index.html', {root: 'client/build/'})
+        return res.sendFile('index.html', {root: '../client/build/'})
     });
 
 const port = process.env.PORT || 5000;
-console.debug(`yaii !! ${port} ${publicPath}/index.html`);
+console.debug(`__dirname ${__dirname}`);
 app.listen(port, () => console.log(`Listening on port ${port}`));
