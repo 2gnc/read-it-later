cadb847765341e1a5838c7ed8cf7f03bf62f57a2
tgnc <tgnc@yandex-team.ru>
2019-08-13 18:15:05 +0300

    снова новые пути

--- server/app.js
+++ server/app.js
@@ -1,18 +1,21 @@
-// const path = require('path')
+const path = require('path');
 const express = require('express');
 // const config = require('./utils/config');
 const userData = require('./api/v1/user-data');
 
 require('./database/index');
 
+const clientPath = path.join(__dirname, '..', 'client/build');
+
 const app = express()
-    .use(express.static('../client/build'))
+    .use(express.static(`${clientPath}`))
     .use(express.json())
     .use(userData)
     .get('*', (req, res) => {
-        return res.sendFile('index.html', {root: '../client/build/'})
+        return res.sendFile(`${clientPath}/index.html`)
     });
 
 const port = process.env.PORT || 5000;
 console.debug(`__dirname ${__dirname}`);
+console.debug('clientPath', clientPath);
 app.listen(port, () => console.log(`Listening on port ${port}`));
